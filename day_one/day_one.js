f162d4b407645d944ee747f509acea320bdc6dfd
Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2022-12-02 02:52:41 +0000

    add part two solution

--- day_one/day_one.js
+++ day_one/day_one.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 async function solve(err, data) {
+  // part one
   if (err) console.log(err);
   else var elfIdx = 0;
   var elfCalories = [[]];
@@ -18,9 +19,19 @@ async function solve(err, data) {
 
   const maximumCalories = Math.max(...elfCaloriesSum);
   const maximumCaloriesIdx = elfCaloriesSum.indexOf(maximumCalories);
+  console.log("[ PART ONE ]");
   console.log("elf with highest calories is:");
   console.log(maximumCaloriesIdx);
   console.log(maximumCalories);
+
+  // part two
+  var elfCaloriesT3 = [];
+  elfCaloriesSum = elfCaloriesSum.sort(function (a, b) {
+    return b - a;
+  });
+  console.log("[ PART TWO ]")
+  console.log("sum of calories of top 3 elves:")
+  console.log(elfCaloriesSum.slice(0, 3).reduce((a, b) => a + b));
 }
 
 fs.readFile("day_one/data.txt", "utf8", solve);
